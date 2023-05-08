93310a733694461fb56bf752ca27299eea32009f
PaulAmann <57676267+PaulAmann@users.noreply.github.com>
2023-05-08 21:32:04 +0200

    changed css dynamic styling to css modules
    css modules is another way to style components. it keeps js and css separate and still scopes the css to the components

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,27 +1,41 @@
-import styled from 'styled-components';
+import React from "react";
 
-const Button = styled.button`
+import styles from './Button.module.css';
+// import styled from 'styled-components';
 
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
+// const Button = styled.button`
 
-  &:focus {
-    outline: none;
-  }
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
 
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`; // this is called a tagged template literal
+//   @media (max-width: 768px) {
+//     width: 100%;
+//   }
 
+//   &:focus {
+//     outline: none;
+//   }
+
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `; // this is called a tagged template literal
+
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 
 export default Button;
